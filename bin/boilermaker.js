8ba5a3d9eaa83a7ee753a4c5242bc5264c3f4984
danhenderson <danhenderson.web@gmail.com>
2016-11-06 07:09:24 +1000

    Changed to a node script

---
+++ bin/boilermaker.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process')
+const { join } = require('path')
+
+const script = process.argv[2]
+const plopfile = join(__dirname, '../plopfile.js')
+
+if (script === 'create') {
+	const proc = spawn(
+		'plop',
+		[ '--plopfile=' + plopfile ],
+		{ stdio: 'inherit' }
+	)
+
+	proc.on('exit', (code, signal) => {
+		process.on('exit', () => {
+			if (signal) {
+				process.kill(process.pid, signal)
+			} else {
+				process.exit(code)
+			}
+		})
+	})
+} else {
+	console.log('Please enter a valid command [create]')
+}
