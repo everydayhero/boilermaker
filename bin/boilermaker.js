2fab7919cf9a1b25e1652c099ac9cd4f7838eb01
Ellis Clayton <ellis@ellisclayton.info>
2016-11-11 15:09:03 +1000

    Standard-ify

--- bin/boilermaker.js
+++ bin/boilermaker.js
@@ -7,21 +7,21 @@ const script = process.argv[2]
 const plopfile = join(__dirname, '../plopfile.js')
 
 if (script === 'create') {
-	const proc = spawn(
-		'plop',
-		[ '--plopfile=' + plopfile ],
-		{ stdio: 'inherit' }
-	)
+  const proc = spawn(
+    'plop',
+    [ '--plopfile=' + plopfile ],
+    { stdio: 'inherit' }
+  )
 
-	proc.on('exit', (code, signal) => {
-		process.on('exit', () => {
-			if (signal) {
-				process.kill(process.pid, signal)
-			} else {
-				process.exit(code)
-			}
-		})
-	})
+  proc.on('exit', (code, signal) => {
+    process.on('exit', () => {
+      if (signal) {
+        process.kill(process.pid, signal)
+      } else {
+        process.exit(code)
+      }
+    })
+  })
 } else {
-	console.log('Please enter a valid command [create]')
+  console.log('Please enter a valid command [create]')
 }
