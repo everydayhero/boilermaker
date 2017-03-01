7ed59a65a6a714aa8ffceb1f56f6f33554a41690
Ellis Clayton <ellis@ellisclayton.info>
2017-03-01 14:47:39 +1000

    Ensure tests pass for new components

--- templates/source/components/Stateless/__tests__/Stateless-test.js
+++ templates/source/components/Stateless/__tests__/Stateless-test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {describe, it} from 'mocha'
 import {shallow} from 'enzyme'
 import {expect} from 'chai'
@@ -7,14 +8,14 @@ import {{pascalCase name}} from '../'
 
 describe('{{pascalCase name}}', () => {
   it('renders a title', () => {
-    subject = shallow(<{{pascalCase name}} title='Test Title'/>)
+    const subject = shallow(<{{pascalCase name}} title='Test Title' />)
     expect(subject.text()).to.eql('Test Title')
   })
 
   it('fires an onClick handler', () => {
     const spyClick = sinon.spy()
-    subject = shallow(<{{pascalCase name}} onClick={spyClick}/>)
+    const subject = shallow(<{{pascalCase name}} onClick={spyClick} />)
     subject.simulate('click')
-    expect(spyClick).to.have.been.called
+    expect(spyClick.callCount).to.equal(1)
   })
 })
