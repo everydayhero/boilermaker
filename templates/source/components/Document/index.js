45a700a0bf8bf17c5d411bd4fbbd2f94295d71d4
danhenderson <danhenderson.web@gmail.com>
2017-08-04 13:55:27 +1000

    Update packages
    - Updated relevant packages e.g. react-router
    - Added supporticon question
    - Added CnvHeaderVersion meta tag

--- templates/source/components/Document/index.js
+++ templates/source/components/Document/index.js
@@ -20,6 +20,7 @@ export default ({
     <head>
       <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='CnvHeaderVersion' content='v5.0' />
       {head.title.toComponent()}
       {head.meta.toComponent()}
       {renderStyles(styles)}
